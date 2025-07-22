e2e1ad5120d182d576467c3fb695e321aa3237c9
jiwonee02 <nepthun@naver.com>
2025-07-22 14:09:09 +0900

    [Feat] 스플래쉬 화면 구현 #2

---
+++ src/styles/user/SplashStyle.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+    background-color: #007aff;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Logo = styled.img`
+    height: 210px;
+    width: auto;
+`;
