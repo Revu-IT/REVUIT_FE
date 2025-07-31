3103e7c3a7e3096fbbf9840230cd72cf0415efaf
jiwonee02 <nepthun@naver.com>
2025-08-01 00:12:01 +0900

    [feat] 마이페이지 구현 #7

--- src/components/PlatformChangeForm.jsx
+++ src/components/PlatformChangeForm.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import * as P from "../styles/components/PlatformChangeFormStyle";
 
 import temu from "../assets/images/temu.svg";
+import gmarket from "../assets/images/gmarket.svg";
+import elevenst from "../assets/images/11st.svg";
+import coupang from "../assets/images/coupang.svg";
+import ali from "../assets/images/ali.svg";
 import checkmark from "../assets/images/checkmark.svg";
 import checkmark2 from "../assets/images/checkmark_selected.svg";
 
@@ -9,33 +13,33 @@ function PlatformChangeForm() {
     return (
         <P.Platform>
             <P.Card>
-                <P.Logo src={temu} />
+                <P.Logo src={elevenst} />
                 <P.Content>
-                    <P.Title>TEMU</P.Title>
+                    <P.Title>11번가</P.Title>
                     <P.Subtitle>플랫폼</P.Subtitle>
                 </P.Content>
                 <P.Check src={checkmark} />
             </P.Card>
             <P.Card>
-                <P.Logo src={temu} />
+                <P.Logo src={ali} />
                 <P.Content>
-                    <P.Title>TEMU</P.Title>
+                    <P.Title>Aliexpress</P.Title>
                     <P.Subtitle>플랫폼</P.Subtitle>
                 </P.Content>
                 <P.Check src={checkmark} />
             </P.Card>
             <P.Card>
-                <P.Logo src={temu} />
+                <P.Logo src={coupang} />
                 <P.Content>
-                    <P.Title>TEMU</P.Title>
+                    <P.Title>쿠팡</P.Title>
                     <P.Subtitle>플랫폼</P.Subtitle>
                 </P.Content>
                 <P.Check src={checkmark2} />
             </P.Card>
             <P.Card>
-                <P.Logo src={temu} />
+                <P.Logo src={gmarket} />
                 <P.Content>
-                    <P.Title>TEMU</P.Title>
+                    <P.Title>G마켓</P.Title>
                     <P.Subtitle>플랫폼</P.Subtitle>
                 </P.Content>
                 <P.Check src={checkmark} />
--- src/pages/user/ProfileSettings.jsx
+++ src/pages/user/ProfileSettings.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import * as C from "../../styles/CommonStyle";
 import * as P from "../../styles/ProfileSettingsStyle";
 
@@ -7,6 +7,16 @@ import Edit from "../../components/EditProfileForm";
 import Change from "../../components/PlatformChangeForm";
 
 function ProfileSettings() {
+    const [step, setStep] = useState(1); // 1: 프로필 입력 단계, 2: 플랫폼 선택 단계
+
+    const handleNext = () => {
+        if (step === 1) {
+            setStep(2); // 다음 단계로 이동
+        } else {
+            // 수정 로직 실행 (예: 저장 API 호출 등)
+            console.log("수정 완료");
+        }
+    };
     return (
         <>
             <C.Page>
@@ -15,12 +25,26 @@ function ProfileSettings() {
                         <Header Title="마이페이지" />
                         <P.Profile>
                             <P.Title>
-                                <span>회원정보</span>를 <br></br>입력해주세요.
+                                {step === 1 ? (
+                                    <>
+                                        <span>회원정보</span>를 <br />
+                                        입력해주세요.
+                                    </>
+                                ) : (
+                                    <>
+                                        리뷰를 확인할
+                                        <br />
+                                        <span>기업</span>을 선택해주세요.
+                                    </>
+                                )}
                             </P.Title>
                             <P.Form>
-                                <Edit />
+                                {step === 1 ? <Edit /> : <Change />}
                             </P.Form>
-                            <P.Button>다음</P.Button>
+
+                            <P.Button onClick={handleNext}>
+                                {step === 1 ? "다음" : "수정"}
+                            </P.Button>
                         </P.Profile>
                     </C.PageSpace>
                 </C.Center>
