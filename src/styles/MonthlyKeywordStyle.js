aaf1160c9dc3d220ca088fa8b0a74b5e8c6138a3
jiwonee02 <nepthun@naver.com>
2025-08-12 10:29:00 +0900

    [feat] 분기별 키워드 페이지 구현 #8

---
+++ src/styles/MonthlyKeywordStyle.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Keyword = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 24px;
+`;
+
+export const Cloud = styled.div``;
+
+export const CloudImg = styled.div``;
+
+export const Container = styled.div``;
+
+export const Title = styled.div``;
+
+export const Date = styled.div``;
+
+export const Subtitle = styled.div``;
+
+export const KeywordCloude = styled.div``;
