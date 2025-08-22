111090054c3587271d1c90b12eec2bc9c2e1e531
jiwonee02 <nepthun@naver.com>
2025-08-22 13:58:58 +0900

    [feat] 분기별 리포트 api 연동 #8

--- src/axios/home.js
+++ src/axios/home.js
@@ -2,6 +2,14 @@ import api from "./instance";
 
 // 전체리뷰 및 업계평균 그래프
 export const getStatistics = async () => {
-    const { data } = await api.get("/main/statistics"); // → /api/main/statistics 로 전송됨
+    const { data } = await api.get("/main/statistics");
     return data;
 };
+
+// 분기별 리포트
+export const getMainReport = async () => {
+    const { data } = await api.get("/main/summary");
+    return data;
+};
+
+export default { getStatistics, getMainReport };
